7333089d6a8e7e47e6f9f7fc9c3ba8a4567d1d1c
mspom <marius1spomer@gmail.com>
2023-02-15 20:59:27 +0100

    use article component in app.js with mock up data

--- App.js
+++ App.js
@@ -11,24 +11,35 @@ import {
   View,
 } from 'react-native';
 
+
+
 function App() {
+  const datas =  
+    {
+        "source": {
+            "id": null,
+            "name": "heise online"
+        },
+        "author": "Ben Schwan",
+        "title": "Mehr Smart-Home-Geräte von Apple: iPad-artiger Controller, neues Apple TV",
+        "description": "Apple will sein Angebot an HomeKit-fähigen Produkten offenbar erweitern. Dazu gehört offenbar eine ganz neue Gerätekategorie.",
+        "url": "https://www.heise.de/news/Mehr-Smart-Home-Geraete-von-Apple-iPad-artiger-Controller-neues-Apple-TV-7463699.html",
+        "urlToImage": "https://heise.cloudimg.io/bound/1200x1200/q85.png-lossy-85.webp-lossy-85.foil1/_www-heise-de_/imgs/18/3/6/8/1/9/4/7/homekit-9e68380798906c74.png",
+        "publishedAt": "2023-01-19T11:08:00Z",
+        "content": "Apple plant offenbar die Erweiterung seines Angebots an Smart-Home-Steuerzentralen. Neben HomePod und Apple TV soll demnächst ein \"Smart Display\" stehen. Außerdem ist für 2024 eine neue Multimediabox… [+2168 chars]"
+    }
   return (
-<Articles/>
+  <ScrollView>
+<Articles title={datas.title} description={datas.description}
+urlToImage={datas.urlToImage} source={datas.source.name}/>
+
+</ScrollView>
 
   );
 }
 
-const stily = StyleSheet.create ({
-  header: {
-    fontSize: 30,
-    color: "blue",
-    marginTop: 30
-  },
-  container: {
-    alignSelf: 'center',
-    paddingTop: 70
-  }
 
-})
+
+
 
 export default App;
--- src/components/articles.js
+++ src/components/articles.js
@@ -1,36 +1,87 @@
 import React from 'react';
+import breakfast from "../assets/breakfast.png";
+import { Card, Divider, Image } from "react-native-elements";
 
 import {
+    ImageBackground,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  Touchable,
+  TouchableOpacity,
   useColorScheme,
   View,
 } from 'react-native';
+import moment from 'moment';
 
-function Articles() {
+
+    
+function Articles({title,urlToImage,description,source,publishedAT}) {
+    
+    const defaultImg = breakfast;
+    
+    const time = moment(publishedAT || moment.now()).fromNow();
+
+    const {noteStyle, featuredTitleStyle} = stily;
+
   return (
-<ScrollView> 
+ 
     <View style={stily.container}>
-    <Text style={stily.header}>HELLO WORLD</Text>
+        <Card>
+            <Text>{title}</Text>
+        <Image style={stily.image} resizeMode="cover" source={{uri : urlToImage}}/>
+            <Text>
+                {description || "Read More.."}
+                
+
+            </Text>
+            <Divider style={{ backgroundColor: '#dfe6e9' }} />
+            <View style={stily.cardFooter}>
+                <Text style={noteStyle}>{source}</Text>
+                <Text style={noteStyle}>{time}</Text>
+            </View>
+
+        </Card>
     </View>
-  </ScrollView>
+    
+
 
   );
 }
 
 const stily = StyleSheet.create ({
-  header: {
-    fontSize: 30,
-    color: "blue",
-    marginTop: 30
+  
+container: {
+    flex: 1,
+ },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+},
+cardFooter: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+},
+noteStyle: {
+    margin: 5,
+    fontStyle: 'italic',
+    color: '#b2bec3',
+    fontSize: 10
   },
-  container: {
-    alignSelf: 'center',
-    paddingTop: 70
-  }
+  featuredTitleStyle: {
+    marginHorizontal: 5,
+    textShadowColor: '#00000f',
+    textShadowOffset: { width: 3, height: 3 },
+    textShadowRadius: 3
+  },
+  image: {
+    width: 400,
+    height: 200,
+    marginRight: 100,
+  },
+
 
 })
 
